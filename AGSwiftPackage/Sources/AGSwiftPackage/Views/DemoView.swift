d61fba0857f21ab6fcb9e0f36702e75e39fbfbea
ajaygirolkar <ajaygirolkar@gmail.com>
2022-05-24 12:04:54 +0530

    Added initial project setup

---
+++ AGSwiftPackage/Sources/AGSwiftPackage/Views/DemoView.swift
@@ -0,0 +1,24 @@
+//
+//  DemoView.swift
+//  
+//
+//  Created by Ajay Girolkar on 24/05/22.
+//
+
+import SwiftUI
+
+public struct DemoView: View {
+    
+    public init () {
+        
+    }
+    public var body: some View {
+        Text("Hello, World!, This is library")
+    }
+}
+
+struct DemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoView()
+    }
+}
