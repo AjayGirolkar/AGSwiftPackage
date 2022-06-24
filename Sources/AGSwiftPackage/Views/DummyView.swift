77489544552d23811005258a1990de995c2b3627
AjayGirolkar <ajaygirolkar@gmail.com>
2022-06-24 20:25:41 +0530

    Added private files

---
+++ Sources/AGSwiftPackage/Views/DummyView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Ajay Girolkar on 24/06/22.
+//
+
+import SwiftUI
+
+private struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
---
+++ Sources/AGSwiftPackage/Views/SecondPrivateView.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ajay Girolkar on 24/06/22.
+//
+
+import Foundation
+
+
+private class SecondPrivateClass {
+    
+    
+    func  privateFunction() {
+        print("private functions")
+    }
+}
